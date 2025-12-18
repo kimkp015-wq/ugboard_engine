e3cc6f726edb567e4006de3ca9fa06a6b4a68d66
Kim Ashraf <kimkp015@gmail.com>
2025-12-18 19:08:05 +0300

    Add regional Top 50 charts for Uganda
    Add regional Top 50 charts for Uganda

---
+++ api/charts/regions.py
@@ -0,0 +1,16 @@
+# Uganda regions mapping
+
+REGIONS = {
+    "central": [
+        "Azawi", "Bebe Cool", "Eddy Kenzo", "Vinka", "Fik Fameica"
+    ],
+    "western": [
+        "Ray G", "Truth 256", "Kahinda", "Shon", "Jowy Landa"
+    ],
+    "eastern": [
+        "Winnie Nwagi", "Spice Diana", "Karole Kasita"
+    ],
+    "northern": [
+        "Bosmic Otim", "Lagum The Rapper"
+    ]
+}
