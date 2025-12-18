03db699738c91280cb91fcb616b3764064013af5
Kim Ashraf <kimkp015@gmail.com>
2025-12-18 12:59:13 +0300

    Fix startup
    Fix FastAPI startup for Railway

--- api/main.py
+++ api/main.py
@@ -16,7 +16,3 @@ def health():
 @app.get("/ingest/youtube")
 def ingest_youtube():
     return fetch_ugandan_music(10)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
